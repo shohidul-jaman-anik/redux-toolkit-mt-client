202738946445f354171df2fa4ec80b7d66728783
Md Anik Hossain <anikh499@gmail.com>
2023-02-28 17:25:55 +0600

    add product done

--- src/features/product/productSlice.js
+++ src/features/product/productSlice.js
@@ -1,16 +1,22 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { fetchProducts, postProduct } from "./productsApi";
 
 const initialState = {
     products: [],
     isLoading: false,
+    postSuccess: false,
     isError: false,
     error: ""
 }
 
 export const getProducts = createAsyncThunk("products/getProduct", async () => {
-    const res = await fetch("http://localhost:5000/products")
-    const data = await res.json()
-    return data.data
+    const products = fetchProducts()
+    return products
+})
+
+export const addProduct = createAsyncThunk("products/addProduct", async (data) => {
+    const products = postProduct(data)
+    return products
 })
 
 const productsSlice = createSlice({
@@ -18,17 +24,36 @@ const productsSlice = createSlice({
     initialState,
     extraReducers: (builder) => {
         builder.addCase(getProducts.pending, (state, action) => {
-            state.isLoading = true,
-                state.isError = false;
+            state.isLoading = true
+            state.isError = false;
         })
         builder.addCase(getProducts.fulfilled, (state, action) => {
-            state.products = action.payload,
-                state.isLoading = false;
+            state.products = action.payload
+            state.isLoading = false;
         })
         builder.addCase(getProducts.rejected, (state, action) => {
-            state.products = [],
-                state.isLoading = false,
-                state.isError = true;
+            state.products = []
+            state.isLoading = false
+            state.isError = true;
+            state.error = action.error.message;
+        })
+
+
+
+        builder.addCase(addProduct.pending, (state, action) => {
+            state.isLoading = true;
+            state.postSuccess = false;
+            state.isError = false;
+        })
+        builder.addCase(addProduct.fulfilled, (state, action) => {
+            state.postSuccess = true;
+            state.isLoading = false;
+        })
+        builder.addCase(addProduct.rejected, (state, action) => {
+            state.products = []
+            state.isLoading = false;
+            state.postSuccess = false
+            state.isError = true;
             state.error = action.error.message;
         })
     }
--- src/pages/Dashboard/AddProduct.js
+++ src/pages/Dashboard/AddProduct.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { addProduct } from "../../features/product/productSlice";
 
 const AddProduct = () => {
   const { register, handleSubmit } = useForm();
+  const dispatch = useDispatch()
 
   const submit = (data) => {
     const product = {
@@ -18,7 +21,7 @@ const AddProduct = () => {
       ],
       spec: [],
     };
-
+    dispatch(addProduct(product))
     console.log(product);
   };
 
